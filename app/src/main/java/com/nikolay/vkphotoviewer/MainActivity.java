8727ad16b85a9031bc500749e1e3533d4b83e17d
Nikolay <Nikolay>
2016-03-29 23:16:33 +0300

    add vk sdk

--- app/src/main/java/com/nikolay/vkphotoviewer/MainActivity.java
+++ app/src/main/java/com/nikolay/vkphotoviewer/MainActivity.java
@@ -3,6 +3,8 @@ package com.nikolay.vkphotoviewer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.vk.sdk.VKSdk;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        VKSdk.initialize(MainActivity.this);
 
     }
 }
