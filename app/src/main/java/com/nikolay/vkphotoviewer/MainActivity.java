c10f08938439ff75e117a156c3a3083dcf5c7dda
Nikolay <Nikolay>
2016-04-25 22:50:17 +0300

    25.04 i understand

--- app/src/main/java/com/nikolay/vkphotoviewer/MainActivity.java
+++ app/src/main/java/com/nikolay/vkphotoviewer/MainActivity.java
@@ -2,87 +2,133 @@ package com.nikolay.vkphotoviewer;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
-
-import com.nikolay.vkphotoviewer.util.Autorize;
 import com.vk.sdk.VKAccessToken;
 import com.vk.sdk.VKAccessTokenTracker;
+import com.vk.sdk.VKCallback;
 import com.vk.sdk.VKScope;
 import com.vk.sdk.VKSdk;
 import com.vk.sdk.api.VKApi;
+import com.vk.sdk.api.VKApiConst;
 import com.vk.sdk.api.VKError;
+import com.vk.sdk.api.VKParameters;
 import com.vk.sdk.api.VKRequest;
 import com.vk.sdk.api.VKResponse;
-import com.vk.sdk.api.httpClient.VKHttpClient;
+import com.vk.sdk.api.methods.VKApiPhotos;
 import com.vk.sdk.api.model.VKApiPhoto;
-import com.vk.sdk.api.model.VKList;
+import com.vk.sdk.api.model.VKAttachments;
 import com.vk.sdk.api.model.VKPhotoArray;
-import com.vk.sdk.util.VKUtil;
+import com.vk.sdk.api.model.VKWallPostResult;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
 TextView textView;
+
+    int length;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-       // webView = (WebView) findViewById(R.id.autorize_view);
-VKSdk.login(this, VKScope.WALL);
-    //   webView.loadUrl(Autorize.getAuthURL(VKScope.WALL + "," + VKScope.PHOTOS));
-     //  webView.setWebViewClient(new AutorizeWebViewClient());
-textView = (TextView)findViewById(R.id.textView1);
-        textView.setText(VKAccessToken.currentToken().accessToken);
+VKSdk.login(this, VKScope.WALL, VKScope.PHOTOS);
 
     }
 
-    class AutorizeWebViewClient extends WebViewClient {
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            super.onPageStarted(view, url, favicon);
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!VKSdk.onActivityResult(requestCode, resultCode, data, new VKCallback<VKAccessToken>() {
+            @Override
+            public void onResult(VKAccessToken res) {
+                textView = (TextView)findViewById(R.id.textView1);
+
+
+
+                    textView.setText(VKAccessToken.currentToken().accessToken);
+
+               // textView.setText(makeRequest());
+
+                final VKRequest request = new VKRequest("photos.get", VKParameters.from(VKApiConst.ALBUM_ID, "wall"));
+
+request.executeWithListener(new VKRequest.VKRequestListener() {
+    @Override
+    public void onComplete(VKResponse response) {
+        super.onComplete(response);
+        JSONArray jsonArray = null;
+        try {
+            jsonArray = response.json.getJSONObject("response").getJSONArray("items");
+            textView.setText(jsonArray.getJSONObject(1).getString("photo_807"));
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            super.onPageFinished(view, url);
-
-if (url.startsWith(Autorize.redirectUri)) {
-
-//VKSdk.customInitialize(MainActivity.this, R.integer.com_vk_sdk_AppId, Autorize.versionApi);
 
 
-
-//startActivity(new Intent(MainActivity.this, PhotosActivity.class));
-}
-        }
     }
 
-    private VKRequest.VKRequestListener mRequestListener = new VKRequest.VKRequestListener() {
-        @Override
-        public void onComplete(VKResponse response) {
-            try {
-                VKList<VKApiPhoto> list = (VKList<VKApiPhoto>) response.parsedModel;
-                VKApiPhoto photo_0 = list.get(0);
-            } catch (Exception e) {
+    @Override
+    public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts) {
+        super.attemptFailed(request, attemptNumber, totalAttempts);
+    }
+
+    @Override
+    public void onError(VKError error) {
+        super.onError(error);
+        textView.setText(error.toString());
+    }
+});
+
             }
-        }
+            @Override
+            public void onError(VKError error) {
 
-        @Override
-        public void onError(VKError error) {
-        }
 
-        @Override
-        public void onProgress(VKRequest.VKProgressType progressType, long bytesLoaded,
-                               long bytesTotal) {
+            }
+        })) {
+            super.onActivityResult(requestCode, resultCode, data);
         }
+    }
 
-        @Override
-        public void attemptFailed(VKRequest request, int attemptNumber, int totalAttempts) {
-        }
-    };
+    private String makeRequest() {
+
+        VKRequest request = new VKRequest("photos.get", VKParameters.from(VKApiConst.ALBUM_ID,"wall"));
+        request.executeWithListener(new VKRequest.VKRequestListener() {
+            @Override
+            public void onComplete(VKResponse response) {
+
+
+                JSONArray jsonArray = null;
+                try {
+                    jsonArray = response.json.getJSONObject("response").getJSONArray("items");
+                     length = jsonArray.length();
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+
+            }
+        });
+     return  Integer.toString(length);
+    }
+
+
 }
